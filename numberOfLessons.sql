d7f26a1676ba33109c90dbe2ecfcb0bb0b0a2a8d
mattls95 <54702191+mattls95@users.noreply.github.com>
2022-12-05 17:36:47 +0100

    Update numberOfLessons.sql

--- numberOfLessons.sql
+++ numberOfLessons.sql
@@ -3,7 +3,7 @@ select TO_CHAR(time, 'MONTH') as month,
 count(*) as all_lessons,
 sum(case when lesson_type = 'Single lesson' then 1 else 0 end) as single_lessons,
 sum(case when lesson_type = 'Group lesson' then 1 else 0 end) as group_lessons,
-sum(case when lesson_type = 'Ensamble' then 1 else 0 end) as ensambles
+sum(case when lesson_type = 'Ensemble' then 1 else 0 end) as ensambles
 FROM lesson WHERE EXTRACT(YEAR FROM time) = '2022' 
 GROUP BY TO_CHAR(time, 'MONTH')
 ORDER BY TO_CHAR(time, 'MONTH') ASC;
